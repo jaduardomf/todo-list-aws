3bf013add740a431accd1f07dec7ba7905e8c673
jaduardomf <jmccnp@gmail.com>
2023-05-27 16:22:34 +0000

    Changed deploy.sh s3 reference

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,4 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --s3-resolve
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-oq296rvag1vq
